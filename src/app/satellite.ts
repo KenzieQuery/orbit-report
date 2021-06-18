98c13ee8750edacee3f66e8990d4072199c099ac
KenzieQuery <william_query@comcast.com>
2021-06-18 17:48:46 -0400

    Step 8 commit

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -1,2 +1,28 @@
 export class Satellite {
+    name: string;
+    orbitType: string;
+    type: string;
+    operational: boolean;
+    launchDate: string;
+
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
+        this.name = name
+        this.type = type
+        this.launchDate = launchDate
+        this.orbitType = orbitType
+        this.operational = operational
+    }
+
+    shouldShowWarning(){
+        let warning: boolean;
+        if (this.type.toUpperCase() === 'SPACE DEBRIS') {
+            warning = true;
+        } else {
+            warning = false;
+        }
+        return warning;
+    }
 }
+
+    
+
